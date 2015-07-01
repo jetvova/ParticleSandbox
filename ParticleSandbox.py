799a2f43a6069666b70f71df536c3d540e41dce1
Vova Ivanov <jetvova@gmail.com>
2015-06-30 21:34:47 -0700

    Multiple particles: (First major update!)
    I've finally added the option of legitimately creating multiple particles, with all of them having random velocity and radius (no random position yet). Now this is finally starting to look as how I envisioned it.

--- ParticleSandbox.py
+++ ParticleSandbox.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
-import pygame, sys
+import pygame, sys, random
 from vector import Vector
 from particle import Particle
 
 #IMPORTANT VARIABLES=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-p1 = Particle()
-p1.mass = 1.0
-p1.pos = Vector(100.0, 100.0)
-p1.vel = Vector(1.0, 0.9)
-p1.radius = 20.0
-p2 = Particle()
-p2.mass = 1.0
-p2.pos = Vector(100.0, 100.0)
-p2.vel = Vector(-1.0, -0.9)
-p2.radius = 20.0
+n = 100
+plist = []
+
+for i in range (0,n):
+    p1 = Particle()
+    p1.mass = 1.0
+    p1.pos = Vector(100.0, 100.0)
+    p1.vel = Vector(random.randint(0,1000)/1000.0, random.randint(0, 1000)/1000.0)
+    p1.radius = random.randint(1, 20)
+    plist.append(p1)
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-#NONIMPORTANT VARIABLES==========================
 
 
 
@@ -38,19 +36,19 @@ pink = (255,200,200)
 
 while True:
     screen.fill(black)
-    p1.draw(screen)
-    p2.draw(screen)
 
-    p1.movement()
-    p1.walls()
-    p2.movement()
-    p2.walls()
+    for i in range (0,n):
+        plist[i].draw(screen)
 
-
-
+    for i in range (0,n):
+        plist[i].movement()
+        plist[i].walls()
 
     pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit(); sys.exit();
+            pygame.quit()
+            sys.exit()
+
+
